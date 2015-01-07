24cc8aa630294d0771a1dff5c5def6942db0223d
William Holt <wtholt27@gmail.com>
2015-01-07 14:20:49 -0500

    modified guessing_game

--- guessing_game1.rb
+++ guessing_game1.rb
@@ -4,26 +4,27 @@ puts "Please guess a number between 1 and 100: "
 
 guess = gets.chomp.to_i
 
-guess_counter = 0
+guesses = 0
 
-
-def game number, guess
+def game number, guess, guesses
 	if guess > number
 		puts "Too high...keep trying!"
 	elsif guess < number
 		puts "Too low...keep trying!"
 	else 
-		puts "Congrats! It took you #{guess_counter} many guesses."
+		game_on = false
+		puts "Congrats! It took you #{guesses} guesses to figure it out!"
 	end
 end
 
 game_on = true
 
 
+
 while game_on
-	game(number, guess)
+	game(number, guess, guesses)
 	guess = gets.chomp.to_i
-	guess_counter += 1	
-end
+	guesses += 1	
+end 
 
 
