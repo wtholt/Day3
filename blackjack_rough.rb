e122a3229b3a2ecf51de418d3d3cf495dbee9353
William Holt <wtholt27@gmail.com>
2015-01-07 23:13:39 -0500

    made improvements to blackjack game

--- blackjack_rough.rb
+++ blackjack_rough.rb
@@ -9,48 +9,97 @@ dealer_hand = []
 @game_on = true
 
 def blackjack cards, player_one, dealer_hand
-	player_one = player_one.flatten.inject(:+)
-	dealer_hand = dealer_hand.flatten.inject(:+)
-	if player_one == 0
+	if player_one.length == 0
 		player_one.push(cards.shuffle.pop(2))
+		player_one = player_one.flatten
 		puts "Player hand - #{player_one}" 
 	end
-	if dealer_hand == 0
+	if dealer_hand.length == 0
 		dealer_hand.push(cards.shuffle.pop(2))
-		puts "Good luck!"
+		dealer_hand = dealer_hand.flatten
 		puts "Dealer hand - #{dealer_hand}"
 	end
-	if dealer_hand <= 17
-		dealer_hand.push(cards.shuffle.pop(1))
-	elsif dealer_hand == 21
-		puts "Dealer wins"
-		@game_on = false
-	elsif dealer_hand > 21
-		puts "Dealer busts"
-		@game_on = false
-	elsif player_one < 21
-		puts "Would you like to hit or stay?"
-		question = gets.chomp
-		if question == "Hit.downcase"
-			player_one.push(cards.shuffle.pop(1))
-		else
-			puts "Player stays"
+end
+
+def dealer cards, dealer_hand
+	dealer_hand = dealer_hand.flatten
+	if dealer_hand.inject(:+) <= 17
+		puts "Dealer hits"
+		hit_dealer_hand = dealer_hand.push(cards.shuffle.pop(1))
+		hit_dealer_hand = hit_dealer_hand.flatten
+		if hit_dealer_hand > 21
+			puts "Dealer busts"
+			@game_on = false
+		elsif hit_dealer_hand <= 17
+			puts "Dealer hits again"
+			again_hit_dealer_hand = again_hit_dealer_hand.flatten
+			again_hit_dealer_hand = hit_dealer_hand.push(cards.shuffle.pop(1))
+			if again_hit_dealer_hand > 21
+				puts "Dealer busts"
+				@game_on = false
+			end
 		end
-	elsif player_one == 21
-		puts "Player wins"
+
+	elsif dealer_hand.inject(:+) == 21
+		puts "Dealer wins!"
+		puts "Dealer hand - #{dealer_hand}"
 		@game_on = false
-	elsif player_one > 21
-		puts "Player busts"
-	elsif player_one > dealer_hand
-		puts "Player wins"
-		@game_on = false
-	elsif player_one < dealer_hand
-		puts "Dealer wins"
+	elsif dealer_hand.inject(:+) > 21
+		puts "Dealer busts!"
+		puts "Dealer hand - #{dealer_hand}"
 		@game_on = false
 	end
 end
 
+def player cards, player_one
+	player_one = player_one.flatten
+	if player_one.inject(:+) < 21
+		puts "Would you like to hit or stay?"
+		question = gets.chomp
+		if question == "hit"
+			player_one.push(cards.shuffle.pop(1))
+			puts "Player hand - #{player_one}"
+		elsif question == "stay"
+			puts "Player hand - #{player_one}"
+
+
+		end
+	elsif player_one.inject(:+) > 21
+		puts "Player busts!"
+		puts "Player hand - #{player_one}"
+		@game_on = false
+	elsif player_one.inject(:+) == 21
+		puts "Player wins!"
+		puts "Player hand - #{player_one}"
+		@game_on = false
+	end
+end
+
+def player_dealer cards, player_one, dealer_hand
+	player_one = player_one.flatten
+	dealer_hand = dealer_hand.flatten
+	if player_one.inject(:+) > dealer_hand.inject(:+)
+		puts "Player wins!"
+		puts "Dealer hand - #{dealer_hand}"
+		puts "Player hand - #{player_one}"
+		@game_on = false
+	elsif player_one.inject(:+) < dealer_hand.inject(:+)
+		puts "Dealer wins!"
+		puts "Dealer hand - #{dealer_hand}"
+		puts "Player hand - #{player_one}"
+		@game_on = false
+	elsif player_one.inject(:+) == dealer_hand.inject(:+)
+		puts "It's a draw!"
+		puts "Dealer hand - #{dealer_hand}"
+		puts "Player hand - #{player_one}"
+	end
+end
+		
+
 
 while @game_on
 	blackjack(cards, player_one, dealer_hand)
+	dealer(cards, dealer_hand)
+	player(cards, player_one)
+	player_dealer(cards, player_one, dealer_hand)
 end
