2c04ab8e78b1bad93b04c821a16e7518ae49c92d
William Holt <wtholt27@gmail.com>
2015-01-19 21:16:14 -0500

    optimized guessing game

--- guessing_game1.rb
+++ guessing_game1.rb
@@ -17,15 +17,16 @@ def game number, guess, guesses
 	end
 end
 
-
-
-
-
 while @game_on
 	puts "Please guess a number between 1 and 100: "
 	guess = gets.chomp.to_i
 	game(number, guess, guesses)
 	guesses += 1
+	if guesses >= 5
+		@game_on = false
+		puts "You are only allowed 5 guesses"
+	else
+	end
 end 
 
 
